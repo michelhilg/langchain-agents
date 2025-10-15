687a7adceb603ba6a1d3c54b4278237710b62dcf
Michel <michelhilg@gmail.com>
2025-10-15 07:59:06 -0300

    Add files

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-pergunta = "Quais os dados da Ana?"
-print(pergunta)
+from langchain.agents import AgentExecutor
+from agente import AgenteOpenAIFunctions
+from dotenv import load_dotenv
+
+load_dotenv()
+
+pergunta = "Quais os dados de Ana?"
+
+agente = AgenteOpenAIFunctions()
+
+executor = AgentExecutor(agent=agente.agente,
+                        tools=agente.tools,
+                        verbose=True)
+resposta = executor.invoke({"input" : pergunta})
+print(resposta)
